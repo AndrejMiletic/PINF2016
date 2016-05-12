888fb861a5baf5c244dab6fd32f43f22d7111b52
AndrejMiletic <miletic_andrej@yahoo.com>
2016-05-12 15:58:13 +0200

    Test
    Mala izmena test fajla

--- src/main/java/com/app/controllers/TestController.java
+++ src/main/java/com/app/controllers/TestController.java
@@ -1,5 +1,7 @@
 package com.app.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,8 @@ public class TestController {
 
 	
 	@RequestMapping(value="/all", method=RequestMethod.GET)
-	public String getAllUsers()
+	public ResponseEntity<String> getAllUsers()
 	{	
-		return "Hello world!";
+		return new ResponseEntity<>("Hello from the other side!", HttpStatus.OK);
 	}
 }
