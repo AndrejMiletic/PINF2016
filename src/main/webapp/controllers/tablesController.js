3ef991aa2b5d65e3864db76347445f14d72ad8c4
andrej <miletic_andrej@yahoo.com>
2016-06-06 18:34:02 +0200

    DTO doradjen malo

---
+++ src/main/webapp/controllers/tablesController.js
@@ -0,0 +1,21 @@
+app.controller('tablesController', ['$scope', '$window', 'tableService', function($scope, $window, tableService){
+
+	function init(){
+		tableService.getAll().then(
+			function(response){
+				$scope.allTableNames = response.data;
+			},
+			function(response){
+				alert("Greska");
+			}
+		);
+	}
+
+	init();
+
+	$scope.setTable = function(table){
+		$scope.current = table;
+		alert(table);
+	}
+	
+}]);
